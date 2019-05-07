50dfccfd808a17dde3814c5d051ca973113aa958
Piotr005 <50376092+Piotr005@users.noreply.github.com>
2019-05-07 19:46:40 +0200

    Add files via upload
    Simple code which takes student's rating in range 0 - 6. If incoming rating is equal 7 programs ends. You can add as many raing in range 0 - 6 as you want to. At the end program calculates average of ratings, min and max value of added ratings. 
    Program contains one class called "Dziennik" with 'rating' List and 4 method to add a single rating, calculate average value of all ratings, return min and max added rating.

---
+++ Dziennik.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dzienniczek
+{
+    class Dziennik
+    {
+        List<float> ratings = new List<float>();
+
+        public Dziennik()//konstruktor
+        {
+
+        }
+
+        public void dodajOcene(float ocena)
+        {
+            ratings.Add(ocena);
+        }
+
+        public float sredniaOcen()
+        {
+            float avg = 0, sum = 0;
+
+            foreach (var ocena in ratings)
+            {
+                sum += ocena;
+            }
+            avg = sum / ratings.Count;
+            return avg;
+        }
+
+        public float minRating()
+        {
+            return ratings.Min();
+        }
+
+        public float maxRating()
+        {
+            return ratings.Max();
+        }
+    }
+}
